f97323c312b0616b5aa8b68f7dd936c046953d9c
amirali26 <amiralishojaee123@gmail.com>
2022-04-11 21:04:41 +0100

    updated client cognito

--- src/utils/env.variables.ts
+++ src/utils/env.variables.ts
@@ -14,8 +14,8 @@ const environmentVars: EnvironmentConfig = {
   REACT_APP_API_URL: window.location.href.includes('solicitor.helpmycase.co.uk')
     ? 'https://dashboard-api.helpmycase.co.uk' : 'http://localhost:8080',
   REACT_APP_COGNITO: {
-    poolId: 'eu-west-1_UKEN4PjSR',
-    clientId: '74oeib76gt4308aiuo2hh9qpq1',
+    poolId: 'eu-west-1_FFqUpWtAO',
+    clientId: 'fj2304n3i3ndvc2o6otcrebvo',
     storage: localStorage,
   },
 };
